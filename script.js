c440c1907c738b730a44c90dcfbfdb0bbfac1434
jearikl <171091509+jearikl@users.noreply.github.com>
2025-04-18 15:57:28 -0700

    adding script.js file

---
+++ script.js
@@ -0,0 +1,32 @@
+//gets input from user
+//let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "))
+// 
+// ;
+
+function add7(gang) {
+    return gang + 7
+  } 
+  
+console.log(add7(10))
+
+
+function multiply(num1,num2) {
+return num1 * num2
+}
+
+
+console.log(multiply(2,2))
+
+
+function capitalize(wurd) {
+return wurd.toUpperCase()
+}
+console.log(capitalize("poop"))
+
+function lastLetter(daLtr) {
+return daLtr.at(-1)
+}
+
+console.log(lastLetter("nolhjkl"))
+
+
