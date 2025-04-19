5017354aaec4299efe3cc004c9ad271e14eb9e05
jearikl <171091509+jearikl@users.noreply.github.com>
2025-04-18 21:48:42 -0700

    updates to script.js

--- script.js
+++ script.js
@@ -3,30 +3,69 @@
 // 
 // ;
 
-function add7(gang) {
-    return gang + 7
-  } 
+let humanScore = 0;
+let computerScore = 0;
+
+
+ playGame()
+ playGame()
+ playGame()
+ playGame()
+ playGame()
+
+
+
+function getRandomNumber(){
+    return Math.random()
+}
+
+function getComputerChoice(){
+ if (getRandomNumber() <= .3333) {
+    return "rock";
+} 
+    else if (getRandomNumber() > .6666) {
+        return "scissors";
+    }
+        else {return "paper"}
+    
+};
+
+console.log("computer: " + getComputerChoice())
+
+function getHumanChoice(){
+    const input = prompt("rock, paper or scissors").toLowerCase;
+    if (input === "rock") {
+        return "rock";
+    } else if (input === "paper") {
+        return "paper";
+    } else if (input === "scissors") {
+        return "scissors";
+    } else {
+        return "Add a valid entry";
+    }
+};
+
+console.log("human: " + getHumanChoice());
+
+
+function playGame(){
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice.toLowerCase() == "rock" && computerChoice == "scissors" ||
+    humanChoice.toLowerCase() == "paper" && computerChoice == "rock" ||  
+    humanChoice.toLowerCase() == "scissors" && computerChoice == "paper") {
+humanScore++;
+console.log("you won that round!");}
+else if 
+(humanChoice.toLowerCase() == "scissors" && computerChoice == "rock" ||
+    humanChoice.toLowerCase() == "rock" && computerChoice == "paper" ||  
+    humanChoice.toLowerCase() == "paper" && computerChoice == "scissors") {
+computerScore++;
+console.log("hahaha you stupid bitch you lost")}
+else  {return "tie";}
+}
+}
   
-console.log(add7(10))
 
-
-function multiply(num1,num2) {
-return num1 * num2
-}
-
-
-console.log(multiply(2,2))
-
-
-function capitalize(wurd) {
-return wurd.toUpperCase()
-}
-console.log(capitalize("poop"))
-
-function lastLetter(daLtr) {
-return daLtr.at(-1)
-}
-
-console.log(lastLetter("nolhjkl"))
-
-
+  const humanChoice = getHumanChoice();
+  const computerChoice = getComputerChoice();
